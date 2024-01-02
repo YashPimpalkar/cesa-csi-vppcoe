31c765946ea87d7e8842ee6bf2162f7507c49e53
YashPimpalkar <vu1s2223002@pvppcoe.ac.in>
2024-01-02 22:18:37 +0530

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const { Schema } = mongoose;
+
+const userSchema=new Schema({
+ name:{
+    type:String,
+    unique:true,
+    required:true
+ },
+ email:{
+    type:String,
+    unique:true,
+    required:true
+ },
+ password:{
+    type:String,
+    required:true
+ },
+ 
+},{timestamps:true}
+);
+
+export default mongoose.models.User || mongoose.model("User",userSchema)
