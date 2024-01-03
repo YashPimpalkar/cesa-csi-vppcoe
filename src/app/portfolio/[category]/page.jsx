88ce8cc9d1c80f36ebb63205586077fbb3286d6f
YashPimpalkar <62347457+YashPimpalkar@users.noreply.github.com>
2024-01-03 13:09:21 +0530

    Update page.jsx

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -14,27 +14,25 @@ if(data){
 return notFound()
 }
 
-const category = ({params}) => {
-  const data=getData(params.category)
+const category = ({ params }) => {
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      {data.map((item)=>(
-
-     
-      <div className={styles.item} key={item.id}>
-        <div className={styles.content}>
-               <h1 className={styles.title}>{item.title}</h1>
-               <p className={styles.desc}>{item.desc}</p>
-               <Button url="#" text="See More" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.imgContainer}>
+            <img src={item.image} alt="" className={styles.img} />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button url="#" text="See More" />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-             <Image src={item.image} alt=" " fill={true} className={styles.img} />
-        </div>
-      </div>
-       ) )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default category
