8da1e258c9c72d0bcbde689aa652e4394c42d547
YashPimpalkar <62347457+YashPimpalkar@users.noreply.github.com>
2024-01-03 15:21:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     images:{
         domains:[
-            "images.pexels.com","via.placeholder.com",
+            "images.pexels.com","via.placeholder.com","cesa-vppcoe.vercel.app","raw.githubusercontent.com"
         ]
     },
  
